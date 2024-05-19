5afe257c249c48410d0bcac19e50cb8c718c4c8d
LewisJassy <jassylewis70@gmail.com>
2024-05-19 23:17:30 +0300

    add comments for easy understanding what the code is doing

--- Binary-Tree/binray_depth_calc.py
+++ Binary-Tree/binray_depth_calc.py
@@ -5,14 +5,14 @@ class TreeNode:
         self.right = right # This is the right child of the node
 
 def max_depth(root: TreeNode)-> int:
-    if root is None:
-        return 0
+    if root is None: # check if root is empty or not
+        return 0 # return 0 if root is empty
     
     else:
-        left_depth = max_depth(root.left)
-        right_depth = max_depth(root.right)
+        left_depth = max_depth(root.left) # recursively call the function to get the left depth
+        right_depth = max_depth(root.right) # recursively call the function to get the right depth
 
-        return max(left_depth, right_depth) + 1
+        return max(left_depth, right_depth) + 1 # return the maximum depth of the tree
     
 if __name__ == "__main__":
     # Constructing the example binary tree
