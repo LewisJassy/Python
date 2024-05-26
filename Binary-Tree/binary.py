4f8aaf5431c369b4f10ea2a87100e963cc4b9e90
LewisJassy <jassylewis70@gmail.com>
2024-05-26 09:10:50 +0300

    update the file

--- Binary-Tree/binary.py
+++ Binary-Tree/binary.py
@@ -1,25 +1,25 @@
 from binarytree import Node, build, tree
-# root = Node(6)
-# root.left = Node(3)
-# root.right = Node(10)
-# root.left.left = Node(1)
-# root.left.right = Node(5)
+root = Node(6)
+root.left = Node(3)
+root.right = Node(10)
+root.left.left = Node(1)
+root.left.right = Node(5)
 
-# print("Binary Tree:", root)
-# print("List of Nodes:", list(root))
-# print("Inoder of node:", root.inorder)
-# print('Size of tree :', root.size) 
-# print('Height of tree :', root.height) 
+print("Binary Tree:", root)
+print("List of Nodes:", list(root))
+print("Inoder of node:", root.inorder)
+print('Size of tree :', root.size) 
+print('Height of tree :', root.height) 
   
-# # Get all properties at once 
-# print('Properties of tree : \n', root.properties) 
+# Get all properties at once 
+print('Properties of tree : \n', root.properties) 
 
-# nodes = [5, 4, 7, 2, None, None, 8, 3 , 0]
-# binary_tree = build(nodes)
-# print("Binary Tree:", binary_tree)
+nodes = [5, 4, 7, 2, None, None, 8, 3 , 0]
+binary_tree = build(nodes)
+print("Binary Tree:", binary_tree)
 
-# print('\nList from binary tree :',  
-#       binary_tree.values) 
+print('\nList from binary tree :',  
+      binary_tree.values) 
 
 binary_tree = tree(height=4, is_perfect=True)
 print("Binary Tree:", binary_tree)
